0b6264461a2d88620b2aa17864e84e4ab8a5d1e4
SerNguyen <phamsynguyen2003@gmail.com>
2023-11-06 10:54:17 +0700

    sua loi search

--- cart.py
+++ cart.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 app.secret_key = "FelixPham"
 sqldbname = "db/data.sqlite"
 
+#khoi tao trang bat dau
 @app.route('/')
 def index():
     conn = sqlite3.connect(sqldbname)
@@ -15,6 +16,7 @@ def index():
         'Searching.html', table=data
     )
 
+#Search func
 @app.route('/searchData', methods=['POST'])
 def searchData():
     search_text = request.form['searchInput']
@@ -27,6 +29,8 @@ def searchData():
         output_message=output_message if search_text else None
     )
 
+
+#loaddatatudb
 def load_data_from_db(search_text):
     if search_text != "":
         conn = sqlite3.connect(sqldbname)
@@ -37,7 +41,7 @@ def load_data_from_db(search_text):
         cursor.execute(sqlcommand)
         data = cursor.fetchall()
         conn.close()
-        if len(data) == 0:
+        if len(data) == 0: #vong lap check game ton tai
             output_message = "No Matching Games Found"
             return data,output_message
         else:
@@ -57,6 +61,8 @@ def load_data_from_db(search_text):
 #                              escape=False)
 #     return html_table
 
+
+#Hien thi cac Game va Add to cart func
 @app.route("/cart/add", methods=['POST'])
 def add_to_cart():
     sqldbname = "db/data.sqlite"
@@ -64,22 +70,22 @@ def add_to_cart():
     quantity = int(request.form["quantity"])
     conn = sqlite3.connect(sqldbname)
     cursor = conn.cursor()
-    cursor.execute("SELECT Name, Pric, Img_url_1"
-                   "FROM mytable WHERE id = ?",
+    cursor.execute("SELECT Name, Pric, Img_url_1 "
+                   "FROM mytable WHERE Name = ?",
                    (product_id,))
     product = cursor.fetchone()
     conn.close()
     product_dict = {
         "id": product_id,
-        "name": product[0],
-        "price": product[1],
-        # "quantity": quantity
+        "Name": product[0],
+        "Pric": product[1],
+        "quantity": quantity
     }
     cart = session.get("cart", [])
     found = False
     for item in cart:
         if item["id"] == product_id:
-            # item["quantity"] += quantity
+            item["quantity"] += quantity
             found = True
             break
     if not found:
@@ -94,6 +100,8 @@ def add_to_cart():
     return outputmessage
 
 
+
+#Update cart
 @app.route("/update_cart", methods=['POST'])
 def update_cart():
     cart = session.get('cart',[])
@@ -109,12 +117,14 @@ def update_cart():
     session['cart']=new_cart
     return redirect(url_for('view_cart'))    
 
-@app.route("/viewcart",methods=["POST"])
+@app.route("/view_cart",methods=["POST"])
 def view_cart():
     current_cart=[]
     if 'cart' in session:
         current_cart = session.get("cart",[])
     return render_template("cart.html",carts=current_cart)
+
+
 # #connect to db
 # def connect_db():
 #     conn = sqlite3.connect(sqldbname)  # Replace 'your_database.db' with your actual database file name
